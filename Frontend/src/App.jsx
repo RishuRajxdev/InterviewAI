768de58aa5e51bea73b9a37d23c0cc8e4d642863
Rishu Raj <rrdebug7@gmail.com>
2026-06-26 23:19:44 +0530

    UI Change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
-import { RouterProvider } from "react-router";
-import { router} from "./app.routes";
-import "./App.css";
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
+
 function App() {
+
   return (
-    <>
-      <RouterProvider router= {router}/> 
-    </>
-  );
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
--- Frontend/src/features/Auth/pages/Login.jsx
+++ Frontend/src/features/Auth/pages/Login.jsx
@@ -1,47 +1,50 @@
-import React from "react";
-import "../auth.form.scss";
+import React,{useState} from 'react'
+import { useNavigate, Link } from 'react-router'
+import "../auth.form.scss"
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
-  return (
-    <main className="min-h-screen w-full flex items-center justify-center bg-[#1a1a1a] p-4">
-    
-  <div className="bg-olive-700 rounded-2xl p-8 max-w-md w-full">
-    <h1 className="text-3xl font-bold text-zinc-800 text-center ">Login</h1>
-    
-    <form className="flex flex-col  gap-4 ">
-      {/* Email Group */}
-      <div className="flex flex-col gap-1 ">
-        <label htmlFor="email" className="text-sm font-medium text-emerald-200">Email</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Enter your email address"
-          className="px-4 py-2 rounded-xl border border-emerald-600 bg-emerald-950 text-white placeholder-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-400"
-        />
-      </div>
 
-      {/* Password Group */}
-      <div className="flex flex-col gap-1">
-        <label htmlFor="password" className="text-sm font-medium text-emerald-200">Password</label>
-        <input 
-          type="password" 
-          id="password" 
-          placeholder="Enter Password" 
-          className="px-4 py-2 rounded-xl border border-emerald-600 bg-emerald-950 text-white placeholder-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-400"
-        />
-      </div>
+    const { loading, handleLogin } = useAuth()
+    const navigate = useNavigate()
 
-      {/* Button */}
-      <button 
-        type="submit" 
-        className="mt-2 w-full h-11 bg-emerald-400 hover:bg-emerald-300 font-semibold text-emerald-950 rounded-xl transition-colors"
-      >
-        Login
-      </button>
-    </form>
-  </div>
-</main>
-  );
-};
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
 
-export default Login;
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await handleLogin({email,password})
+        navigate('/')
+    }
+
+    if(loading){
+        return (<main><h1>Loading.......</h1></main>)
+    }
+
+
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login/Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name='email' placeholder='Enter email address' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name='password' placeholder='Enter password' />
+                    </div>
+                    <button className='button primary-button' >Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
+            </div>
+        </main>
+    )
+}
+
+export default Login
