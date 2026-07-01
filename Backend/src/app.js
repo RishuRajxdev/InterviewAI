1283d3276e2e87a8292ce0812597a3f02bf1727b
Rishu Raj <rrdebug7@gmail.com>
2026-07-02 01:13:06 +0530

    cors error resolve

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://interview-forge-client.vercel.app"],
     credentials: true
 }))
+
+app.get("/", (req, res) => {
+    res.send("InterviewForge is Running !")
+})
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
